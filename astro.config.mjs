0e329b3c7fc68ce9f4f53b825371f5c7d9574904
IvanBarranco0226 <ivanbarranco0226@gmail.com>
2026-08-20 13:08:26 -0600

    fix: configurar base path para cargar estilos en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://.github.io',
-  base: '/',
+  base: '/mi-portafolio',
   vite: {
     plugins: [tailwindcss()]
   },
